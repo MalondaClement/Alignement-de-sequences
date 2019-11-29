1701e214d0fd4aabaf9339e687d1b1641aeac47e
MalondaClement <clementmalonda@yahoo.fr>
2019-11-29 16:58:44 +0100

    Maj sol_1 et rapport

--- Code/sol_1.py
+++ Code/sol_1.py
@@ -11,7 +11,25 @@ def SOL_1 (x, y, T) :
 	
 	while (i >= 1) or (j >= 1) :
 		
-		if (T[i][j]) == (T[i-1][j] + 2) :
+		if (i == 1) :
+			u = x[i-1] + u
+			while (j>=1) :
+				u = "-" + u
+				v = y[j-1] + v
+				j= j -1
+			v = y[j-1] + v
+			return (u, v)
+
+		elif j == 1 :
+			v = y[i-1] + v
+			while (i>=1) :
+				v = "-" + v
+				u = x[i-1] + u
+				i = i -1
+			u = u[i-1] + u
+			return (u, v)
+		
+		elif (T[i][j]) == (T[i-1][j] + 2) :
 			u = x[i-1] + u 
 			v = "-" + v
 			i = i - 1
@@ -27,8 +45,11 @@ def SOL_1 (x, y, T) :
 			i = i - 1
 			j = j - 1
 	return (u, v)
+	
+	
 
 
 if __name__=="__main__" :
-	print (len("ATTGTA"), len ("ATCTTA"))
+	#print (len("ATTGTA"), len ("ATCTTA"))
 	print (SOL_1 ("ATTGTA", "ATCTTA", dist.DIST_1 ("ATTGTA", "ATCTTA")[1] ))
+	print (SOL_1 ("ATCG", "GCTGA", dist.DIST_1 ("ATCG", "GCTGA")[1] ))
